721beddaefe578cd9ed89557c80eb727255b5203
Caio Monsores <caio.monsoress@gmail.com>
2023-05-27 20:54:35 -0300

    renderizando a API na pagina

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { BsFillCartPlusFill, BsFillCartCheckFill } from 'react-icons/bs';
 
-const Store = () => {
+export const Store = () => {
+  const [data, setData] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const fetchApi = async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=iphone';
+    const response = await fetch(url);
+    const objJson = await response.json();
+
+    setData(objJson.results);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const handleClick = (obj) => {
+    const element = cart.find((e) => e.id === obj.id);
+    if (element) {
+      const arrFilter = cart.filter((e) => e.id !== obj.id);
+      setCart(arrFilter);
+    } else {
+      setCart([...cart, obj]);
+    }
+  };
+
   return (
-    <div>Store</div>
-  )
-}
+    <div>
+      <h1>Loja de Iphone </h1>
+      <div>
+        {data.map((item) => (
+          <div key={item.id}>
+            <h4>{item.title}</h4>
+            <img src={item.thumbnail} alt="" />
+            <h4>Preço R$: {item.price}</h4>
+            <button onClick={() => handleClick(item)}>
+              {cart.some((itemCart) => itemCart.id === item.id) ? (
+                <BsFillCartCheckFill />
+              ) : (
+                <BsFillCartPlusFill />
+              )}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Store
-
+export default Store;
