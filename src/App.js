986528657a4ff5a499e723c0be9c59e795fc7312
Erick Gomes Fernandes <erick.gomes.fernandes.egf@gmail.com>
2023-05-27 00:43:35 -0300

    TEste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Batata Palha com Aspargos
+          Batata Palha com Aspargos!
         </p>
         <a
           className="App-link"
